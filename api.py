aa59d65f2a9e41ca521586d1b28495588bbb298f
Viraat <viraat.laldas@gmail.com>
2020-12-20 01:35:42 -0500

    updated with second api settings

--- api.py
+++ api.py
@@ -2,9 +2,11 @@ from rich import print
 import twitter
 from env import *
 
-api = twitter.Api(consumer_key=CONSUMER_KEY,
-                      consumer_secret=CONSUMER_SECRET,
-                      access_token_key=ACCESS_TOKEN,
-                      access_token_secret=ACCESS_TOKEN_SECRET)
+# api of the account you want to copy the "followers" and "following"
+api_1 = twitter.Api(consumer_key=API_KEY_1,
+                      consumer_secret=API_KEY_SECRET_1,
+                      access_token_key=ACCESS_TOKEN_1,
+                      access_token_secret=ACCESS_TOKEN_SECRET_1)
 
 print(api.VerifyCredentials())
+
--- temp_env.py
+++ temp_env.py
@@ -1,4 +1,11 @@
-CONSUMER_KEY=""
-CONSUMER_SECRET=""
-ACCESS_TOKEN=""
-ACCESS_TOKEN_SECRET=""
+# account where you want to copy followers from 
+API_KEY_1=""
+API_KEY_SECRET_1=""
+ACCESS_TOKEN_1=""
+ACCESS_TOKEN_SECRET_1=""
+
+# account where you to copy the followers to
+API_KEY_2=""
+API_KEY_SECRET_2=""
+ACCESS_TOKEN_2=""
+ACCESS_TOKEN_SECRET_2=""
